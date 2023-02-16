39fcff1bb8d57b30e753de07248076d6463e496b
Shichao <xtornado47@gmail.com>
2023-02-16 16:57:53 -0500

    Upload udp code

---
+++ mpu6050_udp/mpu6050_udp.ino
@@ -0,0 +1,132 @@
+#include <Adafruit_MPU6050.h> // IMPORTANT!!! Set MPU6050_DEVICE_ID in Adafruit_MPU6050.h to 0x72 if code below does not work.
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+const char* WIFI_SSID = "esp32";
+const char* WIFI_PASS = "givelife";
+
+Adafruit_MPU6050 mpu;
+WiFiUDP udp;
+IPAddress udpAddr;
+bool client_found = false;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  Wire.setPins(15, 13); // sda, scl
+  while (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+
+    delay(10);
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+
+  // below code is for setting up WIFI AP
+  WiFi.softAP(WIFI_SSID, WIFI_PASS);
+  WiFi.setSleep(false);
+
+  Serial.print("http://");
+
+  // below code is for setting up a WIFI AP
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.println(myIP);
+
+  udp.begin(63000);
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  uint8_t buffer[64];
+  udp.parsePacket();
+
+  if (!client_found && udp.read(buffer, 64) > 0)
+  {
+    Serial.print("Client IP: ");
+    udpAddr = udp.remoteIP();
+    client_found = true;
+    Serial.println(udpAddr);
+  }
+
+  if (client_found)
+  {
+    if (mpu.getEvent(&a, &g, &temp))
+    {
+      String serializedData = String(a.acceleration.x) + ";" + String(a.acceleration.y) + ";" + String(a.acceleration.z) + ";" + String(g.gyro.x) +  ";" + String(g.gyro.y) + ";" + String(g.gyro.z);
+      uint8_t data[64];
+      serializedData.getBytes(data, sizeof(data));
+
+      udp.beginPacket(udpAddr, 63000);
+      udp.write(data, serializedData.length() + 1);
+      udp.endPacket();
+    }
+  }
+}
--- mpu6050_udp/udp.py
+++ mpu6050_udp/udp.py
@@ -6,7 +6,7 @@ PORT = 63000
 
 avg = []
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST, PORT))
+s.bind(('', PORT))
 
 s.sendto(b"p", (HOST, PORT))
 
@@ -16,8 +16,8 @@ for i in range(1000):
     newTime = time.time()
     diff = (newTime - lastTime) * 1000
     avg.append(diff)
-    #print(f"{diff} ms\n")
-    #print(f"{data.decode('utf-8')}\n")
+    print(f"{diff} ms\n")
+    print(f"{data.decode('utf-8')}\n")
     time.sleep(0.001)
         
 print(f"avg delay: {sum(avg) / 1000}")
